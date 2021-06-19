d80a4a25f7f978e8b703c05759fea6d0902d77db
br4e <br4e@protonmail.com>
2021-06-19 12:53:06 -0700

    Update metamask.svg

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -3,4 +3,5 @@ import type { RootState, AppDispatch } from './store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export type AppSelector = TypedUseSelectorHook<RootState>
+export const useAppSelector: AppSelector = useSelector;
