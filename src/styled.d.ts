dcbe96b26116a4d49ae3d812103ea87133d09493
C Nuck <cnuck1@protonmail.com>
2021-07-07 03:00:05 -0700

    Changes to allow the uniswap repo to be imported.

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,18 +1,7 @@
 import { CSSProp } from "styled-components";
 
-/*declare module "styled-components" {
-  export interface DefaultTheme {
-    // Your theme stuff here
-  }
-}*/
-
-/*declare module "react" {
-  interface Attributes {
-    css?: CSSProp;
-  }
-}*/
 declare module "react" {
     interface HTMLAttributes<T> extends DOMAttributes<T> {
-      css?: CSSProp;
+        css?: CSSProp;
     }
-  }
+}
