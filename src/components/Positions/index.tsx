d0199702911e38f5787659a01ba35c348cc6df0f
br4e <br4e@protonmail.com>
2021-09-02 12:44:42 -0700

    Remove 1:1 mint

--- src/components/Positions/index.tsx
+++ src/components/Positions/index.tsx
@@ -1,7 +1,6 @@
 import { useAppSelector as selector } from '../../app/hooks'
 import PositionEditor from './PositionEditor'
 import ExistingPositions from './ExistingPositions'
-import OneToOneMint from './OneToOneMint'
 
 
 export default ({}) => selector(state => state.positionsEditor.open)
@@ -9,6 +8,5 @@ export default ({}) => selector(state => state.positionsEditor.open)
   : (
       <>
         <ExistingPositions />
-        <OneToOneMint />
       </>
     )
