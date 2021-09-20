68a63a3e374789659a95e9b36226ee3d1bf79877
br4e <br4e@protonmail.com>
2021-09-20 15:26:36 -0700

    Complete Market Multicall

--- src/slices/market/index.ts
+++ src/slices/market/index.ts
@@ -1,86 +1,53 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { sliceState, getState, getGenericReducerBuilder } from '../'
-import { unscale } from '../../utils'
 import getContract from '../../utils/getContract'
 import { Market } from "../../utils/typechain/Market"
 import { ProtocolContract } from '../contracts'
 import { getLocalStorage } from '../../utils/index'
-import Multicall from '../../utils/Multicall'
-import { BigNumber } from 'ethers'
+import Multicall, { ResultConversion } from '../../utils/Multicall'
 
+export interface marketArgs {
+  Market: string
+}
 
-  export interface marketArgs {
-    Market: string
+export type marketInfo = {
+  lastPeriodGlobalInterestAccrued: number,
+  collateralizationRequirement: number,
+  minPositionSize: number,
+  twapDuration: number,
+  interestPortionToLenders: number,
+  periodLength: number,
+  firstPeriod: number,
+}
+
+export interface MarketState extends sliceState<marketInfo> {}
+
+export const getMarketInfo = createAsyncThunk(
+  'market/getMarketInfo',
+  async (args: marketArgs) => {
+    const market = getContract(args.Market, ProtocolContract.Market) as Market
+
+    return Multicall(market).execute<marketInfo>({
+      lastPeriodGlobalInterestAccrued: ResultConversion.BigNumberToNumber,
+      collateralizationRequirement: ResultConversion.BigNumberUnscale,
+      minPositionSize: ResultConversion.BigNumberUnscale,
+      twapDuration: ResultConversion.Number,
+      interestPortionToLenders: ResultConversion.BigNumberUnscale,
+      periodLength: ResultConversion.BigNumberToNumber,
+      firstPeriod: ResultConversion.BigNumberToNumber,
+    })
   }
+)
 
-  export type marketInfo = {
-    lastPeriodGlobalInterestAccrued: number,
-    collateralizationRequirement: number,
-    minPositionSize: number,
-    twapDuration: number,
-    interestPortionToLenders: number,
-    periodLength: number,
-    firstPeriod: number,
-  }
+const name = 'market'
 
-  export interface MarketState extends sliceState<marketInfo> {}
+export const marketSlice = createSlice({
+  name,
+  initialState: getState<marketInfo>(getLocalStorage(name, null)) as MarketState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder = getGenericReducerBuilder(builder, getMarketInfo)
+  },
+})
 
-  export const getMarketInfo = createAsyncThunk(
-    'market/getMarketInfo',
-    async (args: marketArgs) => {
-      const market = getContract(args.Market, ProtocolContract.Market) as Market
-
-      const multicall = Multicall(market)
-      multicall.add([
-        'lastPeriodGlobalInterestAccrued',
-        'collateralizationRequirement',
-        'minPositionSize',
-        'twapDuration',
-        'interestPortionToLenders',
-        'periodLength',
-        'firstPeriod',
-      ])
-
-      const [
-        lastPeriodGlobalInterestAccrued,
-        collateralizationRequirement,
-        minPositionSize,
-        twapDuration,
-        interestPortionToLenders,
-        periodLength,
-        firstPeriod,
-      ] = await multicall.execute() as [
-        BigNumber,
-        BigNumber,
-        BigNumber,
-        number,
-        BigNumber,
-        BigNumber,
-        BigNumber,
-      ]
-
-
-      return {
-        lastPeriodGlobalInterestAccrued: lastPeriodGlobalInterestAccrued.toNumber(),
-        collateralizationRequirement: unscale(collateralizationRequirement),
-        minPositionSize: unscale(minPositionSize),
-        twapDuration,
-        interestPortionToLenders: unscale(interestPortionToLenders),
-        periodLength: periodLength.toNumber(),
-        firstPeriod: firstPeriod.toNumber(),
-      }
-    }
-  )
-
-  const name = 'market'
-
-  export const marketSlice = createSlice({
-    name,
-    initialState: getState<marketInfo>(getLocalStorage(name, null)) as MarketState,
-    reducers: {},
-    extraReducers: (builder) => {
-      builder = getGenericReducerBuilder(builder, getMarketInfo)
-    },
-  })
-
-  export default marketSlice.reducer
+export default marketSlice.reducer
--- src/utils/Multicall/index.ts
+++ src/utils/Multicall/index.ts
@@ -1,22 +1,41 @@
 import { Web3Provider } from '@ethersproject/providers'
-import { Contract, utils as ethersUtils, ethers } from 'ethers'
+import { FunctionFragment  } from '@ethersproject/abi'
+import { Contract, utils as ethersUtils, BigNumber } from 'ethers'
 
-import { enforce, first } from '../'
+import { enforce, first, unscale } from '../'
 import getProvider from '../getProvider'
 
 import { TcpMulticallViewOnly } from '../typechain'
 
 import tcpMulticallViewOnlyArtifact from '../artifacts/contracts/mocks/TcpMulticallViewOnly.sol/TcpMulticallViewOnly.json'
 
-interface RawCall {
+interface Call {
   func: string,
+  conversion: ResultConversion,
   args: any[] | undefined,
-}
-
-interface Call extends RawCall {
   inputs: ethersUtils.ParamType[]
   outputs?: ethersUtils.ParamType[]
   encoding: string
+}
+
+export enum ResultConversion {
+  Number,
+  BigNumber,
+  BigNumberToNumber,
+  BigNumberUnscale,
+}
+
+const convertResult = (result: any, conversion: ResultConversion) => {
+  switch(conversion) {
+    case ResultConversion.Number:
+      return result as number
+    case ResultConversion.BigNumber:
+      return result as BigNumber
+    case ResultConversion.BigNumberToNumber:
+      return (result as BigNumber).toNumber()
+    case ResultConversion.BigNumberUnscale:
+      return unscale(result as BigNumber)
+  }
 }
 
 const MULTICALL_ADDRESS = '0x153900C946e33AED5F1ee79C92E149A262E2B1E9'
@@ -24,21 +43,28 @@ const MULTICALL_ADDRESS = '0x153900C946e33AED5F1ee79C92E149A262E2B1E9'
 class Multicall {
   contract: Contract
   calls: Call[] = []
-  result: unknown
+  result: {[key in string]: ReturnType<typeof convertResult>} = {}
   provider: Web3Provider
+  multicall: TcpMulticallViewOnly
+  abiCoder: ethersUtils.AbiCoder
 
   constructor(contract: Contract, provider?: Web3Provider) {
     this.contract = contract
+
     this.provider = provider === undefined
       ? getProvider()
       : provider
+
+    this.multicall = new Contract(
+      MULTICALL_ADDRESS,
+      tcpMulticallViewOnlyArtifact.abi,
+      this.provider,
+    ) as TcpMulticallViewOnly
+
+    this.abiCoder = new ethersUtils.AbiCoder()
   }
 
-  add(calls: string[]) {
-    calls.map(call => this.addCallWithArgs(call, []))
-  }
-
-  addCallWithArgs(func: string, args: any[] = []) {
+  addCallWithArgs(func: string, conversion: ResultConversion, args: any[] = []) {
     const matchingFunctions = Object.values(this.contract.interface.functions).filter(interfaceFunction => interfaceFunction.name === func)
     enforce(matchingFunctions.length >= 1, 'No matching functions found for ' + func)
     enforce(matchingFunctions.length <= 1, 'Multiple matching functions found for ' + func)
@@ -57,33 +83,28 @@ class Multicall {
     this.calls.push({
       func,
       args,
+      conversion,
       inputs,
       outputs,
       encoding: this.contract.interface.encodeFunctionData(func, args)
     })
   }
 
-  async execute() {
-    const multicall = new Contract(
-      MULTICALL_ADDRESS,
-      tcpMulticallViewOnlyArtifact.abi,
-      this.provider,
-    ) as TcpMulticallViewOnly
+  async execute<T extends {}>(calls: {[key in string]: ResultConversion}): Promise<T> {
+    for (const [func, conversion] of Object.entries(calls)) this.addCallWithArgs(func, conversion, [])
 
-    const rawResults = await multicall.all(this.calls.map(
+    const rawResults = await this.multicall.all(this.calls.map(
       call => ({ target: this.contract.address, callData: call.encoding })
     ))
 
-    const abiCoder = new ethersUtils.AbiCoder()
-
-    const multicallResult = rawResults.returnData.map((rawResult, index) => {
-      const resultsArray = Object.values(abiCoder.decode(this.calls[index].outputs!, rawResult))
-      return resultsArray.length === 1
-        ? first(resultsArray)
-        : resultsArray
+    rawResults.returnData.map((rawResult, index) => {
+      const call = this.calls[index]
+      const resultsArray = Object.values(this.abiCoder.decode(call.outputs!, rawResult))
+      enforce(resultsArray.length === 1, 'More than one result')
+      this.result[call.func] = convertResult(first(resultsArray), call.conversion)
     })
-    this.result = multicallResult
-    return multicallResult
+
+    return this.result as unknown as T
   }
 }
 
