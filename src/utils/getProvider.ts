955941598ce0e2ffb42bcb17db0967f21379989e
br4e <br4e@protonmail.com>
2021-09-20 14:02:39 -0700

    Multicall fully functional

--- src/utils/getProvider.ts
+++ src/utils/getProvider.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers'
 
-export default () => window.hasOwnProperty('ethereum') && window.ethereum
-  ? new ethers.providers.Web3Provider(window.ethereum)
-  : null
+export default () => {
+  if (window.hasOwnProperty('ethereum') && window.ethereum) return new ethers.providers.Web3Provider(window.ethereum)
+  else throw 'Provider not found'
+}
