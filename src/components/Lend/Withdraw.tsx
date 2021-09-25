e8ad8387315409dc6098d43a10ea504deea04dd9
br4e <br4e@protonmail.com>
2021-09-25 14:29:51 -0700

    Split lend and withdraw into two renderings

---
+++ src/components/Lend/Withdraw.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react"
+import { Row, Col } from 'react-flexbox-grid'
+import {
+  Button,
+  NumberInput,
+  Dropdown,
+  OnChangeData,
+  TextAreaSkeleton,
+} from 'carbon-components-react'
+import LargeText from '../utils/LargeText'
+import { useAppDispatch, useAppSelector as selector } from '../../app/hooks'
+import { waitForHueBalance, waitForLendHueBalance, waitForMarket, waitForPrices, waitForLiquidations } from '../../slices/waitFor'
+import {  numDisplay }  from '../../utils/'
+import { first } from '../../utils'
+import PositionNumberInput from '../Positions/library/PositionNumberInput';
+import { LendBorrowOptions } from './'
+import InputPicker from './library/InputPicker'
+import { reason } from '../Positions/library/ErrorMessage';
+import PositionMetadata from '../Positions/library/PositionMetadata';
+
+
+
+const Withdraw = ({onSelect}: {onSelect: (option: LendBorrowOptions) => void}) => {
+  const dispatch = useAppDispatch()
+
+  const hueBalance = waitForHueBalance(selector, dispatch)
+  const lendHueBalance = waitForLendHueBalance(selector, dispatch)
+
+  const [amount, setAmount] = useState(0)
+
+  if (
+    hueBalance === null ||
+    lendHueBalance === null
+  ) return <TextAreaSkeleton />
+
+  const onChange = (option: LendBorrowOptions) => {
+    if (option !== LendBorrowOptions.Withdraw) onSelect(option)
+  }
+
+  const newWalletBalance = hueBalance.userBalance - amount
+  const newAccountBalance = lendHueBalance.userBalance + amount
+
+
+  const failures: {[key in string]: reason} = {
+    noValueEntered: {
+      message: 'Please insert a value.',
+      failing: amount === 0 || isNaN(amount),
+      silent: true,
+    },
+    notEnoughInWallet: {
+      message: 'Not enough in wallet.',
+      failing: newWalletBalance < 0,
+    },
+  }
+
+  const failureReasons: reason[] = Object.values(failures)
+  const isFailing = failureReasons.filter(reason => reason.failing).length > 0
+
+  return (
+    <>
+      <div>
+        <LargeText>
+          I have {numDisplay(hueBalance.userBalance, 2)} Hue available to deposit.
+          <div />
+          The current lend APR is {numDisplay(hueBalance.userBalance, 2)} but will vary over time.
+        </LargeText>
+      </div>
+      <LargeText>
+        I want to
+        <InputPicker options={LendBorrowOptions} initialValue={LendBorrowOptions.Withdraw} onChange={onChange} />
+        <PositionNumberInput
+          id="withdrawInput"
+          action={(value: number) => setAmount(value)}
+          value={amount}
+        />
+        Hue.
+      </LargeText>
+      <PositionMetadata items={[
+        {
+          title: 'Current Wallet Balance',
+          value: numDisplay(hueBalance.userBalance, 2) + ' Hue',
+          failing: false
+        },{
+          title: 'New Wallet Balance',
+          value: numDisplay(hueBalance.userBalance - amount, 2) + ' Hue',
+          failing: failures.notEnoughInWallet.failing,
+        },{
+          title: 'Current Hue Lent',
+          value: numDisplay(lendHueBalance.userBalance, 2),
+        },{
+          title: 'New Hue Lent',
+          value: numDisplay(newAccountBalance, 2)
+        },
+      ]} />
+      <div style={{marginTop: 32, marginBottom: 32}}>
+        <Button disabled={isFailing}>
+          Withdraw
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default Withdraw
--- src/components/Lend/index.tsx
+++ src/components/Lend/index.tsx
@@ -13,89 +13,20 @@ import { waitForHueBalance, waitForLendHueBalance, waitForMarket, waitForPrices,
 import {  numDisplay }  from '../../utils/'
 import { first } from '../../utils'
 import PositionNumberInput from '../Positions/library/PositionNumberInput';
+import Lend from './Lend'
+import Withdraw from './Withdraw'
 
 export enum LendBorrowOptions {
   Lend = 'Lend',
-  Borrow = 'Withdraw',
+  Withdraw = 'Withdraw',
 }
 
-const InputPicker = <T extends string, TEnumValue extends string>({
-  options,
-  onChange,
-}:{
-  options: { [key in T]: TEnumValue }
-  onChange: (value: TEnumValue) => void
-}) => {
-  return (
-    <div style={{display: 'inline-block', width: 146}} >
-      <Dropdown
-        ariaLabel="Dropdown"
-        id="Lend_Borrow_Dropdown"
-        items={Object.values(options)}
-        onChange={(data: OnChangeData<TEnumValue>) => {
-          const selectedItem = data.selectedItem
-          if (selectedItem === null || selectedItem === undefined) throw new Error('InputPicker: Unknown value')
-          onChange(selectedItem)
-        }}
-        size="sm"
-        initialSelectedItem={first(Object.values(options))}
-        label='Lend/Borrow options'
-        titleText={<> </>}
-        style={{marginLeft: 8, marginRight: 8}}
-      />
-    </div>
-  )
-}
-
-
-const Lend = () => {
-  const dispatch = useAppDispatch()
-
-  const hueBalance = waitForHueBalance(selector, dispatch)
-  const lendHueBalance = waitForLendHueBalance(selector, dispatch)
-
+const LendOrWithdraw = () => {
   const [selectedOption, setSelectedOption] = useState(first(Object.values(LendBorrowOptions)))
-  const [amount, setAmount] = useState(0)
-  const [showBorrowLend, setShowBorrowLend] = useState(false)
 
-  if (
-    hueBalance === null ||
-    lendHueBalance === null
-  ) return <TextAreaSkeleton />
-
-  const onChange = (option: LendBorrowOptions) => {
-    setAmount(0)
-    setSelectedOption(option)
-  }
-
-  const isFailing = false
-
-  return (
-    <>
-      <div>
-        <LargeText>
-          I have {numDisplay(hueBalance.userBalance, 2)} Hue available to deposit.
-          <div />
-          The current lend APR is {numDisplay(hueBalance.userBalance, 2)} but will vary over time.
-        </LargeText>
-      </div>
-      <LargeText>
-        I want to
-        <InputPicker options={LendBorrowOptions} onChange={onChange} />
-        <PositionNumberInput
-          id="lendInput"
-          action={(value: number) => setAmount(value)}
-          value={amount}
-        />
-        Hue.
-      </LargeText>
-      <div style={{marginTop: 32, marginBottom: 32}}>
-        <Button onClick={() => setShowBorrowLend(true)} disabled={isFailing}>
-          {selectedOption}
-        </Button>
-      </div>
-    </>
-  )
+  return selectedOption === LendBorrowOptions.Lend
+    ? <Lend onSelect={setSelectedOption} />
+    : <Withdraw onSelect={setSelectedOption} />
 }
 
-export default Lend
+export default LendOrWithdraw
