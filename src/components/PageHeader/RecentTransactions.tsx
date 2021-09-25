d8c6041158271712ea185b14afc7c3cc5cd3e56d
br4e <br4e@protonmail.com>
2021-09-25 10:22:52 -0700

    Update RecentTransactions.tsx

--- src/components/PageHeader/RecentTransactions.tsx
+++ src/components/PageHeader/RecentTransactions.tsx
@@ -5,6 +5,7 @@ import SmallLink from '../library/SmallLink'
 import { clearUserTransactions } from '../../slices/transactions'
 import { getSortedUserTxs } from '../utils'
 import { getTxNamePastTense, getTxNamePresentTense, getTxHash } from '../../slices/transactions'
+import { Row, Col } from 'react-flexbox-grid'
 
 const RecentTransactions = () => {
   const dispatch = useAppDispatch()
@@ -15,22 +16,29 @@ const RecentTransactions = () => {
   if (sortedUserTxs.length === 0) return null
 
   const txLinks = sortedUserTxs.map(tx =>
-    <SmallLink
-      key={getTxHash(tx)}
-      onClick={() => alert("tx clicked" + tx.type)}
-      icon={Launch16}>
-      {getTxNamePresentTense(tx.type)}
-    </SmallLink>)
+    <Row>
+      <SmallLink
+        key={getTxHash(tx)}
+        onClick={() => alert("tx clicked" + tx.type)}
+        icon={Launch16}>
+        {getTxNamePresentTense(tx.type)}
+      </SmallLink>
+    </Row>)
 
   return (
     <>
       <div style={{marginTop: '1rem'}}>
         <h4>Recent Transactions</h4>
       </div>
-      {txLinks}
-      <Center style={{marginTop: '1rem'}}>
-        <SmallLink onClick={() => dispatch(clearUserTransactions(userAddress!))}>Clear all</SmallLink>
-      </Center>
+      <Col>
+        {txLinks}
+      </Col>
+      {userAddress !== null
+        ? <Center style={{marginTop: '1rem'}}>
+            <SmallLink onClick={() => dispatch(clearUserTransactions(userAddress))}>Clear all</SmallLink>
+          </Center>
+        : null
+      }
     </>
   )
 }
