971519f4482b1253d871cbe32ebbe62c1522ef4b
br4e <br4e@protonmail.com>
2021-09-26 15:37:08 -0700

    Update ExistingPositions.tsx

--- src/components/Positions/ExistingPositions.tsx
+++ src/components/Positions/ExistingPositions.tsx
@@ -2,7 +2,7 @@ import { DataTableSkeleton, Button } from 'carbon-components-react'
 import AppTile from '../library/AppTile'
 import { useAppDispatch, useAppSelector as selector } from '../../app/hooks'
 import { editorOpened } from '../../slices/positionsEditor'
-import { waitForPositions } from '../../slices/waitFor'
+import { waitForPositions , waitForPrices } from '../../slices/waitFor'
 import Center from '../library/Center'
 import SimpleTable from '../library/SimpleTable'
 import { numDisplay } from '../../utils'
@@ -10,27 +10,34 @@ import { numDisplay } from '../../utils'
 const ExistingPositionsTable = () => {
   const dispatch = useAppDispatch()
   const positions = waitForPositions(selector, dispatch)
+  const priceInfo = waitForPrices(selector, dispatch)
 
   const headers = ['Position ID', 'Debt', 'Collateral']
 
-  if (positions === null) {
+  if (positions === null || priceInfo === null) {
     return <DataTableSkeleton headers={headers.map(header => ({key: header}))} rowCount={3} />
   }
 
   if (Object.values(positions).length === 0) return <Center>There are no positions</Center>
 
-  const rows = Object.values(positions).map(position => ({
-    key: position.id,
-    data: {
-      'Position ID': position.id,
-      'Debt': position.debtCount + ' Hue',
-      'Collateral': numDisplay(position.collateralCount) + ' Eth',
-    },
-    onClick: () => dispatch(editorOpened({
-      positionID: position.id,
-      creating: false,
-    }))
-  }))
+  const rows = Object.values(positions).map(position => {
+    const collateralization = (position.collateralCount * priceInfo.ethPrice) / position.debtCount
+    return {
+      key: position.id,
+      data: {
+        'Position ID': position.id,
+        'Debt': numDisplay(position.debtCount, 2) + ' Hue',
+        'Collateral': numDisplay(position.collateralCount, 2) + ' Eth',
+        'Collateralization Ratio': numDisplay(collateralization * 100, 0) + '%',
+        'Rewards': '~546 TCP',
+        '': 'claim'
+      },
+      onClick: () => dispatch(editorOpened({
+        positionID: position.id,
+        creating: false,
+      }))
+    }
+  })
 
   return <SimpleTable rows={rows} />
 }
