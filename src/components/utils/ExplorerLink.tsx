1191e16b8a530507005deff22d705c6148f63c2a
br4e <br4e@protonmail.com>
2021-09-26 15:03:09 -0700

    Improve wallet modal

--- src/components/utils/ExplorerLink.tsx
+++ src/components/utils/ExplorerLink.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react"
-import { useAppDispatch, useAppSelector as selector } from '../../app/hooks'
+import { useAppSelector as selector } from '../../app/hooks'
 import { ReactNode } from "react";
 import { ChainID } from '../../slices/chainID'
-import Text from './Text'
-import {
-  Link,
-} from 'carbon-components-react'
+import { CarbonIconType } from '@carbon/icons-react'
 import { assertUnreachable, xor } from '../../utils'
+import SmallLink from '../library/SmallLink';
 
 
 const getEtherscanTxLink = (txHash: string, chainID: ChainID) => {
@@ -34,23 +31,21 @@ const getEtherscanAddressLink = (address: string, chainID: ChainID) => {
 const ExplorerLink = ({
   txHash,
   address,
-  size,
   icon,
   children,
 }: {
   txHash?: string,
   address?: string,
-  size?: 'sm' | 'md' | 'lg',
-  icon?: React.ComponentType,
+  icon?: CarbonIconType,
   children: ReactNode
 }) => {
   const chainID = selector(state => state.chainID.chainID)
-  const [visited, setVisited] = useState(false)
 
   if (chainID === null) {
     console.error('ExplorerLink: Chain ID Null')
     return <></>
   }
+
   if (!xor(txHash !== undefined, address !== undefined)) {
     console.error('ExplorerLink: Provide only one of address, txhash')
   }
@@ -59,25 +54,12 @@ const ExplorerLink = ({
     ? getEtherscanTxLink(txHash, chainID)
     : getEtherscanAddressLink(address!, chainID)
 
-
-  const onClick = (e: any) => {
-    e.preventDefault()
-    setVisited(true)
-    window.open(etherscanLink, '_blank');
-  }
+  const onClick = () => window.open(etherscanLink, '_blank');
 
   return (
-    <Link
-      onClick={onClick}
-      href={etherscanLink}
-      target='_blank'
-      visited={visited}
-      renderIcon={icon}
-      size={size}
-      style={{cursor: 'pointer'}}
-    >
+    <SmallLink icon={icon} onClick={onClick}>
       {children}
-    </Link>
+    </SmallLink>
   )
 }
 
