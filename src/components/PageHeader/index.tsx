c2037850d515af5690a74540107bd5fe7f779463
br4e <br4e@protonmail.com>
2021-09-27 12:47:47 -0700

    Better nav prototype

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,17 +1,22 @@
 import { MouseEvent } from 'react'
 import { withRouter, useHistory, useLocation } from 'react-router'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, CSSProperties } from 'react'
 import {
   Header,
   HeaderContainer,
   HeaderName,
   HeaderNavigation,
+  HeaderMenu,
   HeaderMenuButton,
   HeaderMenuItem,
+  OverflowMenu,
+  OverflowMenuItem,
   SideNav,
   SideNavItems,
   HeaderSideNavItems,
+  Button,
 } from 'carbon-components-react'
+import { Menu32, Close32 } from '@carbon/icons-react';
 import { Tab } from '../../App'
 import logo from '../../img/tcp-logo-white.svg'
 
@@ -20,6 +25,7 @@ import NetworkIndicator from '../library/NetworkIndicator';
 
 const PageHeader = () => {
   const [ windowWidth, setWindowWidth ] = useState(window.innerWidth)
+  const [ isMenuOpen, setIsMenuOpen ] = useState(false)
 
   useEffect(() => {
     const handleResize = () => setWindowWidth(window.innerWidth)
@@ -44,34 +50,49 @@ const PageHeader = () => {
     )
   })
 
+  const tabsAsButtons = Object.values(Tab).map((tab, index) => {
+    const link = index === 0 ? '/' : '/' + tab.toLowerCase()
+    const isCurrentPage = location.pathname === link
+    const style: CSSProperties = {
+      width: '100%',
+      backgroundColor: 'transparent',
+    }
+    if (isCurrentPage) style.borderLeft = '3px solid #0f62fe'
+    return (
+      <Button
+        className={isCurrentPage ? 'selectedOption' : ''}
+        key={index}
+        href={link}
+        onClick={navigateToRoute.bind(null, link)}
+        style={style}
+        kind="secondary">
+        {tab}
+      </Button>
+    )
+  })
+
+
+  const smallViewport = windowWidth < 650
+
   return (
     <HeaderContainer
-      render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+      render={() => (
         <Header aria-label="Site Header">
-          <HeaderMenuButton
-            aria-label="Open menu"
-            onClick={onClickSideNavExpand}
-            isActive={isSideNavExpanded}
-          />
-          <SideNav
-            aria-label="Side navigation"
-            expanded={isSideNavExpanded}
-            isPersistent={false}>
-            <SideNavItems>
-              <HeaderSideNavItems>
-                {tabs}
-              </HeaderSideNavItems>
-            </SideNavItems>
-          </SideNav>
+          <OverflowMenu
+            renderIcon={isMenuOpen ? Close32 : Menu32}
+            selectorPrimaryFocus={'.selectedOption'}
+            data-floating-menu-container>
+            {tabsAsButtons}
+          </OverflowMenu>
           <HeaderName href="/" prefix="" className='header_logo'>
             <img src={logo} alt="logo" width={32} height={32} style={{marginRight: 16}}/>
-            {windowWidth < 650 ? null : 'Trustless Currency Protocol'}
+            {smallViewport ? null : 'Trustless Currency Protocol'}
           </HeaderName>
           <HeaderNavigation aria-label="Main Site Navigation Links">
             {tabs}
           </HeaderNavigation>
           <div style={{marginLeft: 'auto', marginRight: 16 }}>
-            {windowWidth < 650 ? null : <NetworkIndicator />}
+            {smallViewport ? null : <NetworkIndicator />}
             <span style={{marginLeft: 12}}>
               <Wallet />
             </span>
