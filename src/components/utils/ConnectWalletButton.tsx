b0b1824abc7e14992da84d6baf47725857dc3c38
br4e <br4e@protonmail.com>
2021-09-27 11:35:44 -0700

    Update ConnectWalletButton.tsx

--- src/components/utils/ConnectWalletButton.tsx
+++ src/components/utils/ConnectWalletButton.tsx
@@ -1,5 +1,5 @@
-import { useAppDispatch } from '../../app/hooks'
 import MetaMaskOnboarding from "@metamask/onboarding"
+import { useAppDispatch, useAppSelector as selector } from '../../app/hooks'
 import { connectWallet } from '../utils/WalletConnection'
 import { Button } from 'carbon-components-react'
 
@@ -9,6 +9,14 @@ const ConnectWalletButton = ({
   mini?: boolean,
 }) => {
   const dispatch = useAppDispatch()
+
+  const wallet = selector(state => state.wallet)
+
+  const text = wallet.connecting
+    ? 'Confirm in Metamask'
+    : (wallet.address !== null
+        ? 'Connected'
+        : 'Connect a Wallet')
 
   const onClick = async () => {
     if (MetaMaskOnboarding.isMetaMaskInstalled()) {
@@ -22,8 +30,8 @@ const ConnectWalletButton = ({
   const sizeAttribute = mini === true ? { size: 'small' } : {}
 
   return (
-    <Button {...sizeAttribute} onClick={onClick}>
-      Connect a Wallet
+    <Button {...sizeAttribute} onClick={onClick} disabled={wallet.connecting}>
+      {text}
     </Button>
   )
 }
