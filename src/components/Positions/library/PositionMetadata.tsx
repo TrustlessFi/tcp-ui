0224138b190160f8ed35260b595a0046274ca564
br4e <br4e@protonmail.com>
2021-09-26 18:50:11 -0700

    Improve metadata display

--- src/components/Positions/library/PositionMetadata.tsx
+++ src/components/Positions/library/PositionMetadata.tsx
@@ -1,27 +1,42 @@
 import { Row, Col } from 'react-flexbox-grid'
 import Text from '../../utils/Text'
 
+type metadata = {value: string, title: string, failing?: boolean}
 
-const PositionMetadata = ({items}: { items: {value: string, title: string, failing?: boolean}[]}) => {
-  const border = '1px solid #404040'
 
+const Metadata = ({item}: {item: metadata}) => {
   const getColor = (isFailing?: boolean) => isFailing ? '#FA4D56' : '#8D8D8D'
 
-  const valuesView = items.map((item, index) => {
-    return (
-      <Col xs key={"PositionMetadata " + index}>
-        <div>
-          <Text monospace size={14} lineHeight="18px" color={getColor(item.failing)} bold>{item.value}</Text>
-        </div>
-        <div>
-          <Text monospace size={14} lineHeight="18px" color={getColor(item.failing)}>{item.title}</Text>
-        </div>
-      </Col>
-    )
-  })
+  return (
+    <>
+      <Row key={item.value + '0'}>
+        <Text monospace size={14} lineHeight="18px" color={getColor(item.failing)} bold>{item.value}</Text>
+      </Row>
+      <Row key={item.value + '1'}>
+        <Text monospace size={14} lineHeight="18px" color={getColor(item.failing)}>{item.title}</Text>
+      </Row>
+    </>
+  )
+}
+
+const PositionMetadata = ({items}: { items: metadata[]}) => {
+  const border = '1px solid #404040'
+
+  const rows: metadata[][] = []
+  console.log({rows})
+
+  for(let i = 0; i < items.length;) {
+    const itemsInRow: metadata[] = []
+    for(let j = 0; j < 4 && i < items.length;) {
+      itemsInRow.push(items[i])
+      i++
+      j++
+    }
+    rows.push(itemsInRow)
+  }
 
   return (
-    <Row style={{
+    <Col style={{
       borderTop: border,
       borderBottom: border,
       paddingTop: 16,
@@ -29,8 +44,13 @@ const PositionMetadata = ({items}: { items: {value: string, title: string, faili
       paddingLeft: 12,
       paddingRight: 12,
     }}>
-      {valuesView}
-    </Row>
+      {rows.map((row, i0) => (
+          <Row key={i0} style={i0 > 0 ? {marginTop: 8} : {}}>
+            {row.map((item, i1) => <Col key={i1} xs={3}><Metadata item={item} /></Col>)}
+          </Row>
+        )
+      )}
+    </Col>
   )
 }
 
