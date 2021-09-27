b04e8d1a434fd4e5399c0888938af6d561abd646
br4e <br4e@protonmail.com>
2021-09-27 11:49:33 -0700

    Add loading logic to withdraw

--- src/components/Lend/Withdraw.tsx
+++ src/components/Lend/Withdraw.tsx
@@ -20,6 +20,8 @@ import { selectionMade } from '../../slices/lendSelection'
 import { getAPR } from './library'
 import ApprovalButton from '../utils/ApprovalButton'
 import { zeroIfNaN } from '../../utils/index';
+import ConnectWalletButton from '../utils/ConnectWalletButton';
+import RelativeLoading from '../library/RelativeLoading';
 
 const Withdraw = () => {
   const dispatch = useAppDispatch()
@@ -31,23 +33,23 @@ const Withdraw = () => {
   const sdi = waitForSDI(selector, dispatch)
   const marketContract = getContractWaitFunction(ProtocolContract.Market)(selector, dispatch)
 
+  const userAddress = selector(state => state.wallet.address)
+
   const [amount, setAmount] = useState(0)
 
-  if (
+  const dataNull =
     hueBalance === null ||
     lendHueBalance === null ||
     market === null ||
     rates === null ||
     sdi === null ||
     marketContract === null
-  ) return <TextAreaSkeleton />
 
-  const apr = getAPR({market, rates, sdi, hueBalance})
-  console.log({apr})
+  const apr = dataNull ? 0 : getAPR({market, rates, sdi, hueBalance})
 
   const onChange = (option: LendBorrowOption) => dispatch(selectionMade(option))
 
-  const lentHueCount = lendHueBalance.userBalance! * market.valueOfLendTokensInHue
+  const lentHueCount = dataNull ? 0 : lendHueBalance.userBalance! * market.valueOfLendTokensInHue
 
   const failures: {[key in string]: reason} = {
     noValueEntered: {
@@ -65,76 +67,81 @@ const Withdraw = () => {
 
   failures['lendHueNotApproved'] = {
     message: 'Withdrawal is not approved.',
-    failing: zeroIfNaN(amount) !== 0 && (lendHueBalance.approval.Market === undefined || !lendHueBalance.approval.Market.approved),
+    failing: dataNull ? false : zeroIfNaN(amount) !== 0 && (lendHueBalance.approval.Market === undefined || !lendHueBalance.approval.Market.approved),
   }
 
   const failureReasons: reason[] = Object.values(failures)
   const isFailing = failureReasons.filter(reason => reason.failing).length > 0
 
-  const convertHueToLendHue = (amount: number) => amount / market.valueOfLendTokensInHue
-  const convertLendHueToHue = (amount: number) => amount * market.valueOfLendTokensInHue
+  const convertHueToLendHue = (amount: number) => dataNull ? 1 : amount / market.valueOfLendTokensInHue
+  const convertLendHueToHue = (amount: number) => dataNull ? 1 : amount * market.valueOfLendTokensInHue
 
-  const openLendDialog = () => {
+  const openWithdrawDialog = () => {
     dispatch(openModal({
       args: {
         type: TransactionType.Withdraw,
         count: convertHueToLendHue(amount),
-        Market: marketContract,
+        Market: marketContract!,
       },
     }))
   }
 
   return (
     <>
-      <div>
+      <div style={{position: 'relative'}}>
+        <RelativeLoading show={userAddress !== null && dataNull} />
+        <div>
+          <LargeText>
+            I have {dataNull ? '-' : numDisplay(convertLendHueToHue(lendHueBalance.userBalance), 2)} Hue available to withdraw.
+            <div />
+            The current lend APR is {numDisplay(apr * 100, 2)}% but will vary over time due to market forces.
+          </LargeText>
+        </div>
         <LargeText>
-          I have {numDisplay(convertLendHueToHue(lendHueBalance.userBalance), 2)} Hue available to withdraw.
-          <div />
-          The current lend APR is {numDisplay(apr * 100, 2)}% but will vary over time due to market forces.
+          I want to
+          <InputPicker
+            options={LendBorrowOption}
+            initialValue={LendBorrowOption.Withdraw}
+            onChange={onChange}
+          />
+          <PositionNumberInput
+            id="lendInput"
+            action={(value: number) => setAmount(value)}
+            value={amount}
+          />
+          Hue.
         </LargeText>
-      </div>
-      <LargeText>
-        I want to
-        <InputPicker
-          options={LendBorrowOption}
-          initialValue={LendBorrowOption.Withdraw}
-          onChange={onChange}
-        />
-        <PositionNumberInput
-          id="lendInput"
-          action={(value: number) => setAmount(value)}
-          value={amount}
-        />
-        Hue.
-      </LargeText>
-      <div style={{marginTop: 36, marginBottom: 30}}>
-        <PositionMetadata items={[
-          {
-            title: 'Current Wallet Balance',
-            value: numDisplay(hueBalance.userBalance, 2) + ' Hue',
-          },{
-            title: 'New Wallet Balance',
-            value: numDisplay(hueBalance.userBalance + amount, 2) + ' Hue',
-          },{
-            title: 'Current Hue Lent',
-            value: numDisplay(lentHueCount, 2),
-          },{
-            title: 'New Hue Lent',
-            value: numDisplay(lentHueCount - amount, 2),
-            failing: failures.notEnoughLent.failing,
-          },
-        ]} />
+        <div style={{marginTop: 36, marginBottom: 30}}>
+          <PositionMetadata items={[
+            {
+              title: 'Current Wallet Balance',
+              value: (dataNull ? '-' : numDisplay(hueBalance.userBalance, 2)) + ' Hue',
+            },{
+              title: 'New Wallet Balance',
+              value: (dataNull ? '-' : numDisplay(hueBalance.userBalance + amount, 2)) + ' Hue',
+            },{
+              title: 'Current Hue Lent',
+              value: numDisplay(lentHueCount, 2),
+            },{
+              title: 'New Hue Lent',
+              value: numDisplay(lentHueCount - amount, 2),
+              failing: failures.notEnoughLent.failing,
+            },
+          ]} />
+        </div>
       </div>
       <ApprovalButton
         disabled={failingDueToNonApprovalReason || zeroIfNaN(amount) === 0}
         token={ProtocolContract.LendHue}
         protocolContract={ProtocolContract.Market}
-        approvalLabels={{waiting: 'Approve Withdraw', approving: 'Approving Withdraw...', approved: 'Withdraw Approved'}}
+        approvalLabels={{waiting: 'Approve Withdraw', approving: 'Approve in Metamask...', approved: 'Withdraw Approved'}}
       />
       <div style={{marginTop: 32, marginBottom: 32}}>
-        <Button disabled={isFailing} onClick={openLendDialog}>
-          Withdraw
-        </Button>
+        {userAddress === null ? <ConnectWalletButton /> :
+          <Button disabled={isFailing} onClick={openWithdrawDialog}>
+            Withdraw
+          </Button>
+        }
       </div>
       <div>
         <ErrorMessage reasons={failureReasons} />
