68c143445275a812c3bcc01fb862544c17405570
Alex Romanak <35388382+Atr@users.noreply.github.com>
2021-11-19 22:02:04 -0800

    Add shell of Proposal Vote modal (#28)
    * Add Description to proposal
    
    * Add shell of Proposal vote modal
    
    * Remove some semicolons
    
    Co-authored-by: Alex Romanak <github.com/atr>

--- src/components/Governance/Proposal.tsx
+++ src/components/Governance/Proposal.tsx
@@ -1,36 +1,55 @@
-import React, { FunctionComponent } from 'react';
-import { Tile } from 'carbon-components-react';
-import { Proposal as IProposal, ProposalState } from '../../slices/proposals';
-import { AppTag } from '../library/AppTag';
-import ProgressBar from '../library/ProgressBar';
+import { FunctionComponent, SyntheticEvent, useState } from 'react'
+import { ClickableTile } from 'carbon-components-react'
+import { Proposal as IProposal, ProposalState } from '../../slices/proposals'
+import { AppTag } from '../library/AppTag'
+import ProgressBar from '../library/ProgressBar'
+import { ProposalVoteModal } from './ProposalVoteModal'
 
 interface ProposalProps {
-  proposal: IProposal;
-  quorum: number;
+  proposal: IProposal
+  quorum: number
 }
 
-// TO DO: Add Description
+const ProposalDescription: FunctionComponent<{ ipfsHash: string }> = ({ ipfsHash }) => {
+  if (!ipfsHash) {
+    return <span>Description TBD</span>
+  }
+  return <a
+    href={`https://gateway.ipfs.io/ipfs/${ipfsHash}`}
+    target="_blank"
+    rel="noopener noreferrer"
+  >Description</a>
+}
+
 const Proposal: FunctionComponent<ProposalProps> = ({ proposal, quorum }) => {
-  const { proposal: p } = proposal;
-  const totalVotes = p.forVotes + p.againstVotes;
+  const [ isProposalVoteOpen, setIsProposalVoteOpen ] = useState<boolean>(false)
+  const { proposal: p } = proposal
+  const totalVotes = p.forVotes + p.againstVotes
+
+  const closeModal = (e: SyntheticEvent) => {
+    setIsProposalVoteOpen(false)
+    e.stopPropagation()
+  }
+
   return (
-    <Tile style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div>
-          <span> Proposal {p.id}: -- Description TBD -- . {p.state} </span>
-          <div style={{ display: 'inline' }}>
-            <AppTag
-              name={p.state}
-              color={p.state === ProposalState.Active ? 'blue' : 'gray'}
-              selected
-            />
-          </div>
+    <ClickableTile style={{ display: 'flex', justifyContent: 'space-between' }} onClick={() => setIsProposalVoteOpen(true)}>
+      <ProposalVoteModal open={isProposalVoteOpen} onRequestClose={closeModal} proposal={proposal} />
+      <div>
+        <span> Proposal {p.id}: <ProposalDescription ipfsHash={p.ipfsHash} />. {p.state} </span>
+        <div style={{ display: 'inline' }}>
+          <AppTag
+            name={p.state}
+            color={p.state === ProposalState.Active ? 'blue' : 'gray'}
+            selected
+          />
         </div>
-        <div style={{ width: '25%', display: 'flex', flexDirection: 'column' }}>
-          <ProgressBar label="Sentiment" amount={p.forVotes} max={totalVotes} />
-          <ProgressBar label="Quorum Progress" amount={p.forVotes} max={quorum} />
-        </div>
-    </Tile>
-  );
+      </div>
+      <div style={{ width: '25%', display: 'flex', flexDirection: 'column' }}>
+        <ProgressBar label="Sentiment" amount={p.forVotes} max={totalVotes} />
+        <ProgressBar label="Quorum Progress" amount={p.forVotes} max={quorum} />
+      </div>
+    </ClickableTile>
+  )
 }
   
-export default Proposal;
+export default Proposal
