a13686d8818da23c517b6e09c6b71294c2a1bf67
br4e <br4e@protonmail.com>
2021-12-07 09:39:11 -0700

    update update positions display

--- src/components/Positions/UpdatePosition.tsx
+++ src/components/Positions/UpdatePosition.tsx
@@ -30,7 +30,9 @@ import ErrorMessage from '../library/ErrorMessage'
 import InputPicker from '../Lend/library/InputPicker'
 import CreateTransactionButton from '../utils/CreateTransactionButton';
 import { Row, Col } from 'react-flexbox-grid'
-import PositionMetadata2 from '../library/PositionMetadata2';
+import PositionMetadata2 from '../library/PositionMetadata2'
+import TwoColumnDisplay from '../utils/TwoColumnDisplay'
+import ParagraphDivider from '../utils/ParagraphDivider'
 
 enum CollateralChange {
   Increase = 'Increase',
@@ -77,19 +79,18 @@ const UpdatePosition = () => {
 
   if (userAddress === null) resetPositions()
 
-  if (
+  const dataNull =
     liquidations === null ||
     hueBalance === null ||
     priceInfo === null ||
+    userEthBalance === null ||
     market === null ||
     rates === null ||
     contracts === null ||
-    userEthBalance === null ||
-    userAddress === null ||
-    positions === null
-  ) return <TextAreaSkeleton />
+    positions === null ||
+    userAddress === null
 
-  const position = positions[positionID]
+  const position = dataNull ? null : positions[positionID]
   const increaseCollateral = collateralChange === CollateralChange.Increase
   const increaseDebt = debtChange === DebtChange.Borrow
 
@@ -98,28 +99,28 @@ const UpdatePosition = () => {
   const debtIncrease = zeroIfNaN(increaseDebt ? debtCount : -debtCount)
   const collateralIncrease = zeroIfNaN(increaseCollateral ? collateralCount : -collateralCount)
 
-  const newDebtCount = (position.debtCount + debtIncrease) < 0 ? 0 : position.debtCount + debtIncrease
-  const newCollateralCount = position.collateralCount + collateralIncrease
+  const newDebtCount = position === null ? 0 : (position.debtCount + debtIncrease) < 0 ? 0 : position.debtCount + debtIncrease
+  const newCollateralCount = position === null ? 0 : position.collateralCount + collateralIncrease
 
-  const collateralization = newDebtCount === 0 ? 0 : (newCollateralCount * priceInfo.ethPrice) / newDebtCount
+  const collateralization = priceInfo === null ? 0 : (newDebtCount === 0 ? 0 : (newCollateralCount * priceInfo.ethPrice) / newDebtCount)
   const collateralizationDisplay = numDisplay(collateralization * 100, 0) + '%'
 
-  const liquidationPrice = (newDebtCount * market.collateralizationRequirement) / (newCollateralCount)
+  const liquidationPrice = market === null ? 0 :  (newDebtCount * market.collateralizationRequirement) / (newCollateralCount)
   const liquidationPriceDisplay = numDisplay(liquidationPrice, 0)
 
-  const totalLiquidationIncentive = (liquidations.discoveryIncentive + liquidations.liquidationIncentive - 1) * 100
+  const totalLiquidationIncentive = liquidations === null ? 0 : (liquidations.discoveryIncentive + liquidations.liquidationIncentive - 1) * 100
 
   // const interestRate = (rates.positiveInterestRate ? rates.interestRateAbsoluteValue : -rates.interestRateAbsoluteValue) * 100
 
-  const failures: {[key in string]: reason} = {
+  const failures: { [key in string]: reason } = {
     noChange: {
       message: 'No change.',
       failing: debtIncrease === 0 && collateralIncrease === 0,
       silent: true,
     },
     notBigEnough: {
-      message: 'Position has less than ' + numDisplay(market.minPositionSize) + ' Hue.' ,
-      failing: 0 < newDebtCount && newDebtCount < market.minPositionSize,
+      message: 'Position has less than ' + numDisplay(market === null ? 0 : market.minPositionSize) + ' Hue.',
+      failing: market === null ? false : 0 < newDebtCount && newDebtCount < market.minPositionSize,
     },
     negativeCollateral: {
       message: 'Position does not have enough Eth.',
@@ -127,148 +128,149 @@ const UpdatePosition = () => {
     },
     insufficientEthInWallet: {
       message: 'Connected wallet does not have enough Eth.',
-      failing: userEthBalance - collateralIncrease < 0,
+      failing: userEthBalance === null ? false : userEthBalance - collateralIncrease < 0,
     },
     insufficientHueInWallet: {
       message: 'Connected wallet does not have enough Hue.',
-      failing: hueBalance.userBalance + debtIncrease < 0,
+      failing: hueBalance === null ? false : hueBalance.userBalance + debtIncrease < 0,
     },
     undercollateralized: {
-      message: 'Position has a collateralization less than ' + numDisplay(market.collateralizationRequirement * 100) + '%.',
-      failing: newDebtCount !== 0 && collateralization < market.collateralizationRequirement,
+      message: 'Position has a collateralization less than ' + numDisplay(market === null ? 0 : market.collateralizationRequirement * 100) + '%.',
+      failing: market === null ? false : newDebtCount !== 0 && collateralization < market.collateralizationRequirement,
     },
     paybackNotApproved: {
       message: 'Paying back Hue is not approved.',
-      failing: debtIncrease < 0 && (hueBalance.approval.Market === undefined || !hueBalance.approval.Market.approved),
+      failing: hueBalance === null ? false : debtIncrease < 0 && (hueBalance.approval.Market === undefined || !hueBalance.approval.Market.approved),
     },
   }
 
   const failureReasons: reason[] = Object.values(failures)
   const isFailing = failureReasons.filter(reason => reason.failing).length > 0
 
-  const paragraphDivider = <div style={{height: 32}} />
-
   const newDebtCountDisplay = numDisplay(newDebtCount < 0 ? 0 : newDebtCount, 2)
 
   const ethPriceDisplay = priceInfo === null ? '-' : numDisplay(priceInfo.ethPrice, 0)
 
-  return (
+  const metadataItems = [
+    {
+      title: 'Position Collateral',
+      value: numDisplay(newCollateralCount, 2) + ' Eth',
+      failing: failures.negativeCollateral.failing,
+    }, {
+      title: 'Position Debt',
+      value: newDebtCountDisplay + ' Hue',
+    }, {
+      title: 'Hue/Eth Current Price',
+      value: ethPriceDisplay,
+      failing: false,
+    }, {
+      title: 'Hue/Eth Liquidation Price',
+      value: liquidationPriceDisplay,
+      failing: priceInfo === null  ? false : liquidationPrice >= priceInfo.ethPrice,
+    }, {
+      title: 'Collateralization Ratio',
+      value: collateralizationDisplay,
+      failing: market === null ? false : collateralization < market.collateralizationRequirement,
+    },
+  ]
+
+
+  const columnOne =
     <>
-      <Breadcrumbs items={[{ text: 'Positions', href: '/' }, 'Update']} />
-      <Row>
-        <Col xs={4}>
-          <Text>
-            <div />
-            <Row bottom='xs' style={{marginBottom: 8}}>
-              <Col>
-                <InputPicker
-                  options={CollateralChange}
-                  initialValue={initialCollateralChange}
-                  onChange ={(option: CollateralChange) => setCollateralChange(option)}
-                />
-              </Col>
-              <Col>
-                Eth collateral by
-              </Col>
-            </Row>
-            <PositionNumberInput
-              id="collateralInput"
-              action={(value: number) => setCollateralCount(value)}
-              value={collateralCount}
-            />
-            <Row bottom='xs' style={{marginBottom: 8}}>
-              <Col>
-                <InputPicker
-                  options={DebtChange}
-                  initialValue={initialDebtChange}
-                  onChange ={(option: DebtChange) => setDebtChange(option)}
-                />
-              </Col>
-              <Col>
-                Hue
-              </Col>
-            </Row>
-            <PositionNumberInput
-              id="debtInput"
-              action={(value: number) => setDebtCount(value)}
-              value={debtCount}
-            />
-          </Text>
-          <div style={{marginTop: 36, marginBottom: 30}}>
-            <PositionMetadata2 items={[
-              {
-                title: 'Position Collateral',
-                value: numDisplay(newCollateralCount, 2) + ' Eth',
-                failing: failures.negativeCollateral.failing,
-              },{
-                title: 'Position Debt',
-                value: newDebtCountDisplay + ' Hue',
-              },{
-                title: 'Hue/Eth Current Price',
-                value: ethPriceDisplay,
-                failing: false,
-              },{
-                title: 'Hue/Eth Liquidation Price',
-                value: liquidationPriceDisplay,
-                failing: liquidationPrice >= priceInfo.ethPrice,
-              },{
-                title: 'Collateralization Ratio',
-                value: collateralizationDisplay,
-                failing: collateralization < market.collateralizationRequirement,
-              },
-            ]} />
-          </div>
-          <div style={{marginTop: 32}}>
-            <CreateTransactionButton
-              title={"Approve Payback"}
-              disabled={debtIncrease >= 0 || hueBalance.approval.Market?.approved}
-              showDisabledInsteadOfConnectWallet={true}
-              shouldOpenTxTab={false}
-              txArgs={{
-                type: TransactionType.ApproveHue,
-                Hue: contracts!.Hue,
-                spenderAddress: contracts!.Market,
-              }}
-            />
-          </div>
-          <div style={{marginTop: 32, marginBottom: 32}}>
-            <CreateTransactionButton
-              title="Update Position"
-              disabled={isFailing}
-              txArgs={{
-                type: TransactionType.UpdatePosition,
-                positionID,
-                debtIncrease,
-                collateralIncrease,
-                Market: contracts!.Market,
-              }}
-            />
-          </div>
-          <ErrorMessage reasons={failureReasons} />
+      <Row bottom='xs' style={{ marginBottom: 8 }}>
+        <Col>
+          <InputPicker
+            options={CollateralChange}
+            initialValue={initialCollateralChange}
+            onChange={(option: CollateralChange) => setCollateralChange(option)}
+          />
         </Col>
-        <Col xs={8} style={{marginTop: 32}}>
-          <LargeText>
-            Position #{positionID} currently has {numDisplay(position.collateralCount, 2)} Eth of Collateral
-            and {numDisplay(position.debtCount, 2)} Hue of debt.
+        <Col>
+          Eth collateral by
+        </Col>
+      </Row>
+      <PositionNumberInput
+        id="collateralInput"
+        action={(value: number) => setCollateralCount(value)}
+        value={collateralCount}
+      />
+      <Row bottom='xs' style={{ marginBottom: 8 }}>
+        <Col>
+          <InputPicker
+            options={DebtChange}
+            initialValue={initialDebtChange}
+            onChange={(option: DebtChange) => setDebtChange(option)}
+          />
+        </Col>
+        <Col>
+          Hue
+              </Col>
+      </Row>
+      <PositionNumberInput
+        id="debtInput"
+        action={(value: number) => setDebtCount(value)}
+        value={debtCount}
+      />
+      <div style={{ marginTop: 36, marginBottom: 30 }}>
+        <PositionMetadata2 items={metadataItems} />
+      </div>
+      <div style={{ marginTop: 32 }}>
+        <CreateTransactionButton
+          title={"Approve Payback"}
+          disabled={debtIncrease >= 0 || hueBalance === null || hueBalance.approval.Market ?.approved}
+          showDisabledInsteadOfConnectWallet={true}
+          shouldOpenTxTab={false}
+          txArgs={{
+            type: TransactionType.ApproveHue,
+            Hue: contracts === null ? '' : contracts.Hue,
+            spenderAddress: contracts === null ? '' : contracts.Market,
+          }}
+        />
+      </div>
+      <div style={{ marginTop: 32, marginBottom: 32 }}>
+        <CreateTransactionButton
+          title="Update Position"
+          disabled={isFailing}
+          txArgs={{
+            type: TransactionType.UpdatePosition,
+            positionID,
+            debtIncrease,
+            collateralIncrease,
+            Market: contracts === null ? '' : contracts.Market,
+          }}
+        />
+      </div>
+      <ErrorMessage reasons={failureReasons} />
+    </>
 
-            {paragraphDivider}
+  const columnTwo =
+    <LargeText>
 
-            You are going to {collateralChange.toLowerCase()} collateral
+      Position #{positionID} currently has {position === null ? 0 : numDisplay(position.collateralCount, 2)} Eth of Collateral
+            and {numDisplay(position === null ? 0 : position.debtCount, 2)} Hue of debt.
+
+      <ParagraphDivider />
+
+      You are going to {collateralChange.toLowerCase()} collateral
             by {numDisplay(collateralCount)} Eth for a new total
             of {numDisplay(newCollateralCount, 2)} Eth of collateral
             and {debtChange.toLowerCase()} {numDisplay(debtCount)} Hue for a new total
             of {newDebtCountDisplay} Hue debt in position #{positionID}.
 
-            {paragraphDivider}
+      <ParagraphDivider />
 
-            The price of Eth is currently {numDisplay(priceInfo.ethPrice, 0)} Hue.
+      The price of Eth is currently {priceInfo === null ? 0 : numDisplay(priceInfo.ethPrice, 0)} Hue.
             If the price of Eth falls below <Bold>{liquidationPriceDisplay}</Bold> Hue
             you could lose <Bold>{numDisplay(totalLiquidationIncentive, 0)}%</Bold> or more of your position value in Eth to liquidators.
 
           </LargeText>
-        </Col>
-      </Row>
-    </>
+  return (
+    <TwoColumnDisplay
+      columnOne={columnOne}
+      columnTwo={columnTwo}
+      loading={userAddress !== null && dataNull}
+      breadCrumbItems={[{ text: 'Positions', href: '/' }, 'New']}
+    />
   )
 }
 
