6a071a42c9cd0868c1219a99754aac1996ab146b
br4e <br4e@protonmail.com>
2021-12-13 07:41:48 -0700

    Update ErrorMessage.tsx

--- src/components/library/ErrorMessage.tsx
+++ src/components/library/ErrorMessage.tsx
@@ -9,16 +9,27 @@ export type reason = {
 
 const ErrorMessage = ({
   reasons,
+  large,
 }: {
   reasons: reason[],
+  large: boolean
 }) => {
   const reason = firstOrNull(reasons.filter((reason) => reason.failing && reason.silent !== true))
 
+  if (reason === null) return null
+
   return (
-    <div style={{height: 32}}>
-      <LargeText color='#FA4D56'>{reason === null ? ' ' : reason.message}</LargeText>
+    <div style={{height: 32, color: '#FA4D56'}}>
+      {
+        large
+        ? <LargeText>{reason.message}</LargeText>
+        : <>{reason.message}</>
+      }
     </div>
   )
+}
+ErrorMessage.defaultProps = {
+  large: true
 }
 
 export default ErrorMessage
