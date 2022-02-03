6aaa3469c910d1b68c2c3abe469858fe13022e36
C Nuck <cnuck1@protonmail.com>
2022-02-03 12:49:37 -0800

    Switch to using external react-jazzicon package.

--- src/components/library/WalletButton.tsx
+++ src/components/library/WalletButton.tsx
@@ -1,7 +1,7 @@
 import { useHistory, useLocation } from 'react-router-dom'
 import { abbreviateAddress } from '../../utils'
 import { Tag } from 'carbon-components-react'
-import jazzicon from '@metamask/jazzicon'
+import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
 import { CSSProperties } from 'react'
 
 const WalletButton = ({
@@ -14,21 +14,13 @@ const WalletButton = ({
   const history = useHistory()
   const location = useLocation()
 
-  // Convert '0x...' (hexadecimal) wallet address string to an integer.
-  const radix = 16
-  const diameter = 24
-  const identicon = jazzicon(diameter, parseInt(address.slice(2, 10), radix))
-
   const inTransactions = location.pathname.includes('transactions')
 
   return (
     <Tag
       onClick={inTransactions ? () => {} : () => history.push('/transactions')}
       style={{cursor: inTransactions ? 'default' : 'pointer', ...style}}>
-      <div
-        style={{display: 'inline-block', verticalAlign: 'middle', marginTop: 4, marginRight: 4}}
-        dangerouslySetInnerHTML={{ __html: identicon.outerHTML }}
-      />
+      <Jazzicon diameter={24} seed={jsNumberForAddress(address)} paperStyles={{marginRight: '4px', verticalAlign: 'middle'}} />
       <div style={{display: 'inline', verticalAlign: 'middle', fontSize: 16, fontWeight: 400}}>
         {abbreviateAddress(address)}
       </div>
