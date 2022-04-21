3911bb2950811c4e4563a9ea51790db8ecaa5fdc
br4e <br4e@protonmail.com>
2022-04-20 19:37:12 -0700

    remove spicy for now

--- src/components/library/ComposeTweetButton.tsx
+++ src/components/library/ComposeTweetButton.tsx
@@ -16,7 +16,7 @@ import { Button, ButtonKind} from 'carbon-components-react'
 export enum TweetType {
   Mild,
   Medium,
-  Spicy,
+  // Spicy,
 }
 
 export const tweets: {[tweetType in TweetType]: string} =
@@ -25,8 +25,8 @@ export const tweets: {[tweetType in TweetType]: string} =
     '@trustlessfi is building the future of Defi on zkSync. Try out the fully functional zkSync demo: trustless.fi/discord',
   [TweetType.Medium]:
     '@trustlessfi is reinventing airdrops. Tcp genesis distributes tokens equally regardless of wealth. Join the community: trustless.fi/discord',
-  [TweetType.Spicy]:
-    'Defi is centralized: DAI is wrapped USDC, COMP is controlled by 4 VCs, and TERRA\'s collateral is off-chain. @trustlessfi is building the future of defi on zkSync. Join the community: trustless.fi/discord',
+  // [TweetType.Spicy]:
+    // 'Defi is centralized: DAI is wrapped USDC, COMP is controlled by 4 VCs, and TERRA\'s collateral is off-chain. @trustlessfi is building the future of defi on zkSync. Join the community: trustless.fi/discord',
 }
 
 const tokens: {[token in string]: ReactNode} = {
