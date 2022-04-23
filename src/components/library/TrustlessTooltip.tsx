b978259e2545e0820b657c91dd6b71eea5842629
br4e <br4e@protonmail.com>
2022-04-22 18:16:49 -0700

    improve tooltips

--- src/components/library/TrustlessTooltip.tsx
+++ src/components/library/TrustlessTooltip.tsx
@@ -1,6 +1,8 @@
 import { useState, CSSProperties } from 'react'
 import { Information16 } from '@carbon/icons-react'
 import { Tile } from 'carbon-components-react'
+import { isMobile } from 'react-device-detect'
+import { timeMS } from '../../utils/'
 
 export const TrustlessTooltip = ({
   text,
@@ -9,13 +11,25 @@ export const TrustlessTooltip = ({
   text: string
   style?: CSSProperties
 }) => {
-  const [isActive, setIsActive]  = useState(false)
+  const [isActive, setIsActive] = useState(false)
+  const [lastOpen, setLastOpen] = useState(0)
+
+  const TAP_BUFFER = 250
+
+  const updateActive = (activeNext: boolean) => {
+    if (isMobile) {
+      if (activeNext) setLastOpen(timeMS())
+      else if (timeMS() - TAP_BUFFER < lastOpen) return
+    }
+    setIsActive(activeNext)
+  }
 
   return (
     <div
       style={{position: 'relative', display: 'inline-block', cursor: 'pointer'}}
-      onMouseEnter={() => setIsActive(true)}
-      onMouseLeave={() => setIsActive(false)} >
+      onClick={() => updateActive(!isActive)}
+      onMouseEnter={() => updateActive(true)}
+      onMouseLeave={() => updateActive(false)} >
       <Information16 width={15} style={{opacity: '80%'}} />
       {
         isActive
@@ -23,12 +37,12 @@ export const TrustlessTooltip = ({
           light
           style={{
             position: 'absolute',
-            top: '-100%',
-            left: '125%',
+            top: '125%',
+            left: '-125%',
             zIndex: 10000,
             ...style,
           }}>
-            <div style={{width: 300,lineHeight: 1.5}}>
+            <div style={{width: 250,lineHeight: 1.5}}>
               {text}
             </div>
           </Tile>
