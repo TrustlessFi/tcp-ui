a59463f59f8fa013a04acf52a534927cb4d07156
br4e <br4e@protonmail.com>
2022-04-22 18:33:11 -0700

    Update SocialLinks.tsx

--- src/components/library/SocialLinks.tsx
+++ src/components/library/SocialLinks.tsx
@@ -8,6 +8,7 @@ import {
   Launch32,
 } from '@carbon/icons-react'
 import SpacedList from './SpacedList'
+import Center from './Center'
 import ComposeTweetButton from './ComposeTweetButton'
 import {
   Link,
@@ -17,6 +18,7 @@ import TrustlessLogos, { TrustlessLogoColor } from './TrustlessLogos'
 import { Button, ButtonSize } from 'carbon-components-react'
 import { Tab, tabToPath } from '../../App'
 import waitFor from '../../slices/waitFor'
+import { isMobile } from 'react-device-detect'
 
 export const SocialLink = ({
   icon,
@@ -45,43 +47,73 @@ const SocialLinks = () => {
   }
   */
 
+  const shareButton =
+    <Button
+      onClick={() => window.open('https://twitter.com/trustlessfi/status/1516941542329622532')}
+      renderIcon={Launch32}
+      size='small'>
+      Retweet Trustless
+    </Button>
+
+
+  const siteLink =
+    <SocialLink
+      icon={
+        <img
+          alt="Trustless Finance"
+          src={convertSVGtoURI(TrustlessLogos[TrustlessLogoColor.White]['TDao'])}
+          width={32}
+          height={32}
+        />
+      }
+      href='https://www.trustless.fi/'
+    />
+
+  const discordLink =
+    <SocialLink
+      icon={<LogoDiscord32 />}
+      href='http://discord.gg/pNxCph5CKk'
+    />
+
+  const twitterLink =
+    <SocialLink
+      icon={<LogoTwitter32 />}
+      href='https://twitter.com/trustlessfi'
+    />
+
+  const githubLink =
+    <SocialLink
+      icon={<LogoGithub32 />}
+      href='https://github.com/trustlessfi'
+    />
+
   return (
-    <SpacedList
-      row
-      spacing={10}
-      style={{ position: 'fixed', right: 10, bottom: 10, zIndex: 1000 }}>
-      <span style={{marginRight: 10}}>
-        <Button
-          onClick={() => window.open('https://twitter.com/trustlessfi/status/1516941542329622532')}
-          renderIcon={Launch32}
-          size='small'>
-          Retweet Trustless
-        </Button>
-      </span>
-      <SocialLink
-        icon={
-          <img
-            alt="Trustless Finance"
-            src={convertSVGtoURI(TrustlessLogos[TrustlessLogoColor.White]['TDao'])}
-            width={32}
-            height={32}
-          />
-        }
-        href='https://www.trustless.fi/'
-      />
-      <SocialLink
-        icon={<LogoDiscord32 />}
-        href='http://discord.gg/pNxCph5CKk'
-      />
-      <SocialLink
-        icon={<LogoTwitter32 />}
-        href='https://twitter.com/trustlessfi'
-      />
-      <SocialLink
-        icon={<LogoGithub32 />}
-        href='https://github.com/trustlessfi'
-      />
-    </SpacedList>
+    isMobile
+    ? <SpacedList spacing={20} style={{marginBottom: 20}}>
+        <Center>
+          {shareButton}
+        </Center>
+        <Center>
+          <SpacedList row spacing={10}>
+            {siteLink}
+            {discordLink}
+            {twitterLink}
+            {githubLink}
+          </SpacedList>
+        </Center>
+      </SpacedList>
+    : <SpacedList
+        row
+        spacing={10}
+        style={{ position: 'fixed', right: 10, bottom: 10, zIndex: 1000 }}>
+        <span style={{marginRight: 10}}>
+          {shareButton}
+        </span>
+        {siteLink}
+        {discordLink}
+        {twitterLink}
+        {githubLink}
+      </SpacedList>
   )
 }
 
